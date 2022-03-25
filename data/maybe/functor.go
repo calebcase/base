f6b9964c87f88bc290b65c568dc73924afa7038d
Caleb Case <calebcase@gmail.com>
2022-03-24 22:20:55 -0400

    Make the functor function names more Go-like

--- data/maybe/functor.go
+++ data/maybe/functor.go
@@ -1,6 +1,6 @@
 package maybe
 
-func Fmap[A, B any](f func(A) B, v Type[A]) Type[B] {
+func FMap[A, B any](f func(A) B, v Type[A]) Type[B] {
 	if j, ok := v.(Just[A]); ok {
 		return Just[B]{f(j.Value)}
 	}
@@ -8,7 +8,7 @@ func Fmap[A, B any](f func(A) B, v Type[A]) Type[B] {
 	return Nothing[B]{}
 }
 
-func Freplace[A, B any](a A, v Type[B]) Type[A] {
+func FReplace[A, B any](a A, v Type[B]) Type[A] {
 	if _, ok := v.(Just[B]); ok {
 		return Just[A]{a}
 	}
--- data/maybe/functor_test.go
+++ data/maybe/functor_test.go
@@ -6,22 +6,22 @@ import (
 	"github.com/calebcase/base/data/maybe"
 )
 
-func ExampleFmap() {
+func ExampleFMap() {
 	flip := func(a bool) bool {
 		return !a
 	}
 
-	flop := maybe.Fmap[bool, bool](flip, maybe.Just[bool]{true})
+	flop := maybe.FMap[bool, bool](flip, maybe.Just[bool]{true})
 	fmt.Println(flop)
 
 	stringify := func(a bool) string {
 		return fmt.Sprint(a)
 	}
 
-	str := maybe.Fmap[bool, string](stringify, maybe.Just[bool]{true})
+	str := maybe.FMap[bool, string](stringify, maybe.Just[bool]{true})
 	fmt.Println(str)
 
-	fmt.Println(maybe.Fmap[bool, string](stringify, maybe.Nothing[bool]{}))
+	fmt.Println(maybe.FMap[bool, string](stringify, maybe.Nothing[bool]{}))
 
 	// Output:
 	// {false}
@@ -29,10 +29,10 @@ func ExampleFmap() {
 	// {}
 }
 
-func ExampleFreplace() {
-	fmt.Println(maybe.Freplace[bool, bool](false, maybe.Just[bool]{true}))
-	fmt.Println(maybe.Freplace[string, bool]("done", maybe.Just[bool]{true}))
-	fmt.Println(maybe.Freplace[string, bool]("done", maybe.Nothing[bool]{}))
+func ExampleFReplace() {
+	fmt.Println(maybe.FReplace[bool, bool](false, maybe.Just[bool]{true}))
+	fmt.Println(maybe.FReplace[string, bool]("done", maybe.Just[bool]{true}))
+	fmt.Println(maybe.FReplace[string, bool]("done", maybe.Nothing[bool]{}))
 
 	// Output:
 	// {false}
